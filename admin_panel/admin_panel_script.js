b24685317e76a259fe2e2ea128a51aa948dd3029
Crazzynel <nakanosan@gmx.fr>
2024-04-10 01:32:04 +0200

    updating some pages to fix bugs

---
+++ admin_panel/admin_panel_script.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    // Récupérer l'ID de licence depuis localStorage
+    var licenceCode = localStorage.getItem('licence');
+    var licenceCodeDate = localStorage.getItem('date')
+
+    // Récupérer les éléments HTML
+    var versionElement = document.getElementById('version');
+    var descriptionElement = document.getElementById('description');
+    var appNameElement = document.getElementById('appName');
+    var userSessionElement = document.getElementById('userSession');
+    var userAddressElement = document.getElementById('userAddress');
+    var licenceIdElement = document.getElementById('licenceId');
+    var licenceIdDate = document.getElementById("dateLicence");
+
+    // Assigner les valeurs aux éléments HTML
+    versionElement.textContent = getAppVersion();
+    descriptionElement.textContent = getAppDescription();
+    appNameElement.textContent = getAppName();
+    userSessionElement.textContent = getUserSession();
+    userAddressElement.textContent = getUserAddress();
+    licenceIdElement.textContent = licenceCode;
+    licenceIdDate.textContent= licenceCodeDate;
+};
+
+function getAppVersion() {
+    return "1.0.0 Bêta - Developer Mode"; // Remplacer par la fonction ou variable qui récupère la version de l'application
+}
+
+function getAppDescription() {
+    return "Application de Scouting destinée à l'équipe de robotique CyborgBulls pour la participation à l'évènement FRC 2024 - Crescendo"; // Remplacer par la fonction ou variable qui récupère la description de l'application
+}
+
+function getAppName() {
+    return "CyborgBulls Scouting - APP 2024 patch 1"; // Remplacer par la fonction ou variable qui récupère le nom de l'application
+}
+
+function getUserSession() {
+    return "Accès refusé"; // Remplacer par la fonction ou variable qui récupère le nom de l'utilisateur
+}
+
+function getUserAddress() {
+    return "Code d'accès bloqué "; // Remplacer par la fonction ou variable qui génère l'adresse utilisateur -> 
+}
+
+//function getLicenceId() {
+//    return licenceCode; // Remplacer par la fonction ou variable qui récupère l'ID de licence
+//}
--- index.js
+++ index.js
@@ -15,19 +15,14 @@ function navigateToMatchScouting() {
     // Ajoute le code pour naviguer vers la page de scouting match
     console.log('Navigating to Match Scouting');
     window.location.href="./execute_scout/match/test.html"
-    // win
 }
 
 function navigatetoAdminPanel() {
-        console.error("Impossible d'acceder a la base de donnees admin"),
-        alert("Connexion bloquee  par un administrateur")
+        window.location.href="./admin_panel/page.html"        
       }
 
 /// CHAMP ADMIS POUR LES CONSOLE.LOG - LOGS MANUEL permettant de se retrouver dans les modes de developpement.
 console.error("Licence: Absente")
 
 /// FIN DE CHAMP
-
-
-
 // Team list is stocked on the folder named "teams"
